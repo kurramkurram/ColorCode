95729503180786911d651e530f27359c0807085f
Masataka.Goto <54581188+kurramkurram@users.noreply.github.com>
2020-06-20 12:38:13 +0900

    add slider

--- lib/view/Main.dart
+++ lib/view/Main.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Main extends StatefulWidget {
@@ -6,19 +9,53 @@ class Main extends StatefulWidget {
 }
 
 class _MainState extends State<Main> {
+  var _ratingR = 0.0;
+  var _ratingG = 0.0;
+  var _ratingB = 0.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Flutter Demo Home Page"),
+        title: Text("Color Code"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have Pushed the button this many times:',
+            Text('R : $_ratingR'),
+            Slider(
+              value: _ratingR,
+              activeColor: Colors.red,
+              onChanged: (newRating) {
+                setState(() => _ratingR = newRating);
+              },
+              min: 0,
+              max: 255,
+              divisions: 255,
             ),
+            Text('G : $_ratingG'),
+            Slider(
+              value: _ratingG,
+              activeColor: Colors.green,
+              onChanged: (newRating) {
+                setState(() => _ratingG = newRating);
+              },
+              min: 0,
+              max: 255,
+              divisions: 255,
+            ),
+            Text("B: $_ratingB"),
+            Slider(
+              value: _ratingB,
+              activeColor: Colors.blue,
+              onChanged: (newRating) {
+                setState(() => _ratingB = newRating);
+              },
+              min: 0,
+              max: 255,
+              divisions: 255,
+            )
           ],
         ),
       ),
@@ -30,5 +67,10 @@ class _MainState extends State<Main> {
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  @override
+  void setState(VoidCallback fn) {
+    super.setState(fn);
   }
 }
