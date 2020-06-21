670d6565f7f05954e6a505d68807f058a335583e
Masataka.Goto <54581188+kurramkurram@users.noreply.github.com>
2020-06-21 11:48:15 +0900

    set canvas color

--- lib/view/Main.dart
+++ lib/view/Main.dart
@@ -9,64 +9,72 @@ class Main extends StatefulWidget {
 }
 
 class _MainState extends State<Main> {
-  var _ratingR = 0.0;
-  var _ratingG = 0.0;
-  var _ratingB = 0.0;
+  var _ratingR = 255.0;
+  var _ratingG = 255.0;
+  var _ratingB = 255.0;
+
+  Scaffold _scaffold;
+  Positioned _positioned;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Color Code"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('R : $_ratingR'),
-            Slider(
-              value: _ratingR,
-              activeColor: Colors.red,
-              onChanged: (newRating) {
-                setState(() => _ratingR = newRating);
-              },
-              min: 0,
-              max: 255,
-              divisions: 255,
-            ),
-            Text('G : $_ratingG'),
-            Slider(
-              value: _ratingG,
-              activeColor: Colors.green,
-              onChanged: (newRating) {
-                setState(() => _ratingG = newRating);
-              },
-              min: 0,
-              max: 255,
-              divisions: 255,
-            ),
-            Text("B: $_ratingB"),
-            Slider(
-              value: _ratingB,
-              activeColor: Colors.blue,
-              onChanged: (newRating) {
-                setState(() => _ratingB = newRating);
-              },
-              min: 0,
-              max: 255,
-              divisions: 255,
-            )
-          ],
+    return MaterialApp(
+        theme: ThemeData(
+          canvasColor: Color.fromARGB(
+              255, _ratingR.toInt(), _ratingG.toInt(), _ratingB.toInt()),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context).pushNamed("/setting");
-        },
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Color Code"),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('R : $_ratingR'),
+                Slider(
+                  value: _ratingR,
+                  activeColor: Colors.red,
+                  onChanged: (newRating) {
+                    setState(() => _ratingR = newRating);
+                  },
+                  min: 0,
+                  max: 255,
+                  divisions: 255,
+                ),
+                Text('G : $_ratingG'),
+                Slider(
+                  value: _ratingG,
+                  activeColor: Colors.green,
+                  onChanged: (newRating) {
+                    setState(() => _ratingG = newRating);
+                  },
+                  min: 0,
+                  max: 255,
+                  divisions: 255,
+                ),
+                Text("B: $_ratingB"),
+                Slider(
+                  value: _ratingB,
+                  activeColor: Colors.blue,
+                  onChanged: (newRating) {
+                    setState(() => _ratingB = newRating);
+                  },
+                  min: 0,
+                  max: 255,
+                  divisions: 255,
+                )
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed("/setting");
+            },
+            tooltip: 'Increment',
+            child: Icon(Icons.settings),
+          ), // This trailing comma makes auto-formatting nicer for build methods.
+        ));
   }
 
   @override
