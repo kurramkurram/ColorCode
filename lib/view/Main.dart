577db3ceb980c4aa707b39b271149b25c6efaefe
Masataka.Goto <54581188+kurramkurram@users.noreply.github.com>
2020-06-26 20:47:51 +0900

    format

--- lib/view/Main.dart
+++ lib/view/Main.dart
@@ -25,67 +25,68 @@ class _MainState extends State<Main> {
     setColorCodeHex(_ratingR, _ratingG, _ratingB);
     _controller.text = _colorCodeHex;
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
-      appBar: AppBar(
-        title: Text("Color Code"),
-      ),
-      body: Container(
-        color: Color.fromARGB(
-            255, _ratingR.toInt(), _ratingG.toInt(), _ratingB.toInt()),
-        padding: EdgeInsets.all(40),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            TextFormField(
-              controller: _controller,
-              onChanged: (e) {
-                updateColor(e);
-              },
+          appBar: AppBar(
+            title: Text("Color Code"),
+          ),
+          body: Container(
+            color: Color.fromARGB(
+                255, _ratingR.toInt(), _ratingG.toInt(), _ratingB.toInt()),
+            padding: EdgeInsets.all(40),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                TextFormField(
+                  controller: _controller,
+                  onChanged: (e) {
+                    updateColor(e);
+                  },
+                ),
+                Text('R : $_ratingR'),
+                Slider(
+                  value: _ratingR,
+                  activeColor: Colors.red,
+                  onChanged: (newRating) {
+                    setState(() => _ratingR = newRating);
+                  },
+                  min: 0,
+                  max: 255,
+                  divisions: 255,
+                ),
+                Text('G : $_ratingG'),
+                Slider(
+                  value: _ratingG,
+                  activeColor: Colors.green,
+                  onChanged: (newRating) {
+                    setState(() => _ratingG = newRating);
+                  },
+                  min: 0,
+                  max: 255,
+                  divisions: 255,
+                ),
+                Text("B: $_ratingB"),
+                Slider(
+                  value: _ratingB,
+                  activeColor: Colors.blue,
+                  onChanged: (newRating) {
+                    setState(() => _ratingB = newRating);
+                  },
+                  min: 0,
+                  max: 255,
+                  divisions: 255,
+                )
+              ],
             ),
-            Text('R : $_ratingR'),
-            Slider(
-              value: _ratingR,
-              activeColor: Colors.red,
-              onChanged: (newRating) {
-                setState(() => _ratingR = newRating);
-              },
-              min: 0,
-              max: 255,
-              divisions: 255,
-            ),
-            Text('G : $_ratingG'),
-            Slider(
-              value: _ratingG,
-              activeColor: Colors.green,
-              onChanged: (newRating) {
-                setState(() => _ratingG = newRating);
-              },
-              min: 0,
-              max: 255,
-              divisions: 255,
-            ),
-            Text("B: $_ratingB"),
-            Slider(
-              value: _ratingB,
-              activeColor: Colors.blue,
-              onChanged: (newRating) {
-                setState(() => _ratingB = newRating);
-              },
-              min: 0,
-              max: 255,
-              divisions: 255,
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context).pushNamed("/setting");
-        },
-        tooltip: 'Increment',
-        child: Icon(Icons.settings),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    ));
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed("/setting");
+            },
+            tooltip: 'Increment',
+            child: Icon(Icons.settings),
+          ), // This trailing comma makes auto-formatting nicer for build methods.
+        ));
   }
 
   @override
@@ -100,10 +101,8 @@ class _MainState extends State<Main> {
   }
 
   void updateColor(String colorCode) {
-    print("updateColor colorCode = $colorCode");
     colorCode = colorCode.toUpperCase();
     bool isMatch = RegExp(r'[0-9A-F]{6}').hasMatch(colorCode);
-    print("updateColor isMatch = $isMatch");
     if (isMatch) {
       setState(() => updateColorValue(colorCode));
       _controller.text = _colorCodeHex;
